94f652a25bb4a1ffef33d0ecaacc88c1a6c8f242
naatbarcelos <naathalia.b@hotmail.com>
2024-03-26 22:29:44 -0300

    Schemas - Fim da aula 2

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
 import { UsuarioModel } from '../../models/UsuarioModels';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
+
+
+import md5 from 'md5';
 
 const endpointCadastro = 
     async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
@@ -20,7 +24,19 @@ const endpointCadastro =
         if(!usuario.senha || usuario.senha.length < 4){
             return res.status(400).json({erro : 'Senha invalida'});
         }
-        await UsuarioModel.create(usuario);
+        
+        //validacao se ja existe usuario com o mesmo email 
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+            return res.status(400).json({erro : 'Ja existe uma conta com o email informado'});
+        }
+
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg : 'Usuario criado com sucesso'});
        
     }
@@ -28,4 +44,4 @@ const endpointCadastro =
 
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg'
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModels';
 
-const endpointLogin = (
+
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' &&
-            senha === 'admin@123'){
-                return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha :md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0];
+            return res.status(200).json({msg : `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`});
             }
             return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
     }
@@ -20,3 +24,5 @@ const endpointLogin = (
 }
 
 export default conectarMongoDB(endpointLogin);
+
+//npm run dev 
